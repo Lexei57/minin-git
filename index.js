c8af2076f46181af0aacc50d80abaa4cb479ad37
Lexei57 <mnzllexei@gmail.com>
2022-08-21 20:36:24 +0300

    end merge

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 console.log("Hello world");
 
-<<<<<<< HEAD
-const userAge = 15
-if (age >= 18) {
-    console.log('Совершеннолетний')
-} else {
-    console.log('Несовершеннолетний')
-=======
+
 const age = 20
 
 if (age >= 18) {
     console.log('Совершеннолетний');
 } else {
     console.log('Несовершеннолетний');
->>>>>>> feature/use-main
 }
